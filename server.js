95efd76670ecf6c8e0a5a867855726c199daa5a4
davidebottone <d.bottone@messagegroup.it>
2024-07-18 18:27:29 +0200

    fix percorso immagini

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, 'public/uploads/');
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname);
